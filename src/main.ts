f93e19abba0033fb36e70a90e9b2e99d188b7faf
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-05-01 16:01:01 -0300

    Update

--- src/main.ts
+++ src/main.ts
@@ -7,23 +7,24 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = new DocumentBuilder()
-  .setTitle('Blog Pessoal')
-  .setDescription('Projeto Blog Pessoal')
-  .setContact("Rafael Queiróz","https://github.com/rafaelq80","rafaelproinfo@gmail.com")
-  .setVersion('1.0')
-  .addBearerAuth()
-  .build();
+    .setTitle('Blog Pessoal')
+    .setDescription('Projeto Blog Pessoal')
+    .setContact("Rafael Queiróz", "https://github.com/rafaelq80", "rafaelproinfo@gmail.com")
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('/swagger', app, document);
-  
+
   //process.env.TZ = '-03:00';
-  process.env.TZ = 'America/Sao_Paulo';
+  //process.env.TZ = 'America/Sao_Paulo';
+  process.env.TZ = 'UTC';
 
   app.useGlobalPipes(new ValidationPipe());
 
   app.enableCors();
 
   await app.listen(process.env.PORT || 4000);
-  
+
 }
 bootstrap();
